200a3f44dce2608516cacd8db37e3f32752976c5
sirjan <sirjan.gnr@gmail.com>
2021-11-27 01:02:16 +0545

    middle portion

--- src/component/middle.js
+++ src/component/middle.js
@@ -1,10 +1,71 @@
 import React  from "react";
-
+import "../css/middle.css";
 
 function Middle() {
   return (
     <div className="Middle">
-     <h1>this is middle portion</h1>
+  <div className="middle_name">
+    Team Turon
+  </div>
+  <div className="conversation">
+    <div className="user_reply">
+      <div className="user_reply_avatar">
+        <img src="./assets/avatar1.png" />
+      </div>
+      <div className="user_reply_message_container">
+        <div className="user_reply_message">
+        We’re not having standups today. I have no internet since midnight 
+        </div>
+        <div className="user_reply_timestamp">
+         @robert, 11:43 pm
+        </div>
+
+      </div>
+    </div>
+    <div className="user_reply">
+      <div className="user_reply_avatar">
+        <img src="./assets/avatar1.png" />
+      </div>
+      <div className="user_reply_message_container">
+        <div className="user_reply_message">
+        We’re not having standups today. I have no internet since midnight 
+        </div>
+        <div className="user_reply_timestamp">
+         @robert, 11:43 pm
+        </div>
+
+      </div>
+    </div>
+    <div className="user_reply">
+      <div className="user_reply_avatar">
+        <img src="./assets/avatar2.png" />
+      </div>
+      <div className="user_reply_message_container">
+        <div className="user_reply_message">
+        @sasha Can you send screenshots of the screens you’re working rn?
+        </div>
+        <div className="user_reply_timestamp">
+         @pena, 11:43 pm
+        </div>
+
+      </div>
+    </div>
+    <div className="user_conversation">
+      <div className="user_conversation_message_container">
+      <div className="user_conversation_message">
+        Okay! wait a sec
+      </div>
+      </div>
+      <div className="user_conversation_timestamp">
+        30 mins ago
+      </div>
+    </div>
+  </div>
+<div className="message">
+  <div className="message_input">
+    <input placeholder="Message Team Turon" type="text" />
+  </div>
+  </div>
     </div>
   );
 }
